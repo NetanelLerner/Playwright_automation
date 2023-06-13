2a0c6d10a660912fbbd3969d91e59692e873061a
Netanel Lerner <netanel.l@siklu.com>
2023-06-13 17:39:32 +0300

    add reports

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('simple basic test', async ({ page }) =>{
+test('simple basic test @myTag', async ({ page }) =>{
     await page.goto("https://www.way2automation.com/")
     const pageTitle = await page.locator('#post-17 > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-1e537621.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div > div > section > div > div.elementor-column.elementor-col-25.elementor-inner-column.elementor-element.elementor-element-b3fcb42 > div > div.elementor-element.elementor-element-ee9b1e1.elementor-view-default.elementor-mobile-position-top.elementor-vertical-align-top.elementor-widget.elementor-widget-icon-box > div > div > div.elementor-icon-box-content > h3 > span')
     await expect(pageTitle).toContainText(' Corporate Training ')
@@ -15,7 +15,7 @@ test('simple basic test2', async ({ page }) =>{
 
 
 // assertions
-test('simple basic test2', async ({ page }) =>{
+test.skip('simple basic test3', async ({ page }) =>{
     await page.goto("https://www.amazon.com/")
     await expect(page).toHaveURL("https://www.amazon.com/")
     await expect(page).toHaveTitle("its need to be under the head tag")
@@ -24,4 +24,31 @@ test('simple basic test2', async ({ page }) =>{
     await expect(element).toHaveText("some text")
     await expect(element).toHaveCount(1)
     await expect(element).not.toBeVisible()
+})
+
+test.describe("My first test suit", () => {
+    test('simple basic test', async ({ page }) =>{
+        await page.goto("https://www.way2automation.com/")
+        const pageTitle = await page.locator('#post-17 > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-1e537621.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div > div > section > div > div.elementor-column.elementor-col-25.elementor-inner-column.elementor-element.elementor-element-b3fcb42 > div > div.elementor-element.elementor-element-ee9b1e1.elementor-view-default.elementor-mobile-position-top.elementor-vertical-align-top.elementor-widget.elementor-widget-icon-box > div > div > div.elementor-icon-box-content > h3 > span')
+        await expect(pageTitle).toContainText(' Corporate Training ')
+    })
+    
+    // send keys and click
+    test.skip('simple basic test234', async ({ page }) =>{
+        await page.goto("https://www.amazon.com/")
+        await page.type('#twotabsearchtextbox', "guitar")
+        await page.click('#nav-search-submit-button')
+    })
+
+    test('Screenshot', async ({ page }) =>{
+        await page.goto("https://www.amazon.com/")
+        await page.type('#twotabsearchtextbox', "guitar")
+        await page.screenshot({ path: "screenshot.png", fullPage: true})
+    })
+
+    test('single element Screenshot', async ({ page }) =>{
+        await page.goto("https://www.amazon.com/")
+        const element = await page.$('#twotabsearchtextbox')
+        await element?.screenshot({ path: "single_element_Screenshot.png"})
+    })
 })
